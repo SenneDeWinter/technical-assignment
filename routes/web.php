c8fe8310652b5a6ac95a17a85ac675918847954e
SenneDeWinter <senne.dewinter@student.kdg.be>
2024-07-03 16:09:35 +0200

    🚀✨ Added list view for comics

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\ComicController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ComicController::class, 'getComics']);

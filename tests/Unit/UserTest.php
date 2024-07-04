e0e638759df1fc81003891a3b866b7e56e9ed868
SenneDeWinter <senne.dewinter@student.kdg.be>
2024-07-04 18:30:22 +0200

    🚀✅ Added unit test for creating a new user.

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Models\User;
+
+it('creates a new user', function () {
+    $user = User::factory()->create();
+
+    $this->assertDatabaseHas('users', [
+        'email' => $user->email,
+    ]);
+});

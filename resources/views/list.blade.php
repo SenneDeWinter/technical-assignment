75d5074b25b4d1fd0e58bfbae6200f40731d717d
SenneDeWinter <senne.dewinter@student.kdg.be>
2024-07-04 11:44:33 +0200

    🔄 ✨ Add functionality to add comics to cart via button click

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -22,12 +22,12 @@
                 </a>
                 <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Format: {{$comic->format}}</p>
                 <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Page count: {{$comic->page_count}}</p>
-                <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <button onclick="addToCart({{$comic->marvel_id}})"  class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     Add to cart
                     <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                     </svg>
-                </a>
+                </button>
             </div>
         </div>
     @endforeach
@@ -35,5 +35,21 @@
 <div class="mt-8">
     {{ $comics->links() }}
 </div>
+<script>
+    function addToCart(comicId) {
+        fetch(`/add-to-cart/${comicId}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRF-TOKEN': '{{ csrf_token() }}'
+            },
+        })
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    }
+</script>
 </body>
 </html>

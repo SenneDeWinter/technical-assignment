d558e062dc7028f59f7df29b798713b5f5e74c29
SenneDeWinter <senne.dewinter@student.kdg.be>
2024-07-04 12:59:28 +0200

    🚀 Add and update cart routes for better functionality

--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,7 @@ Route::get('/', [ComicController::class, 'showComics']);
 
 Route::get('/list-comics', [ComicController::class, 'listComics']);
 Route::post('/add-to-cart/{comic_id}', [CartController::class, 'addToBasket']);
-Route::get('/cart', [CartController::class, 'getCart']);
+Route::post('/remove-from-cart/{comic_id}', [CartController::class, 'removeFromBasket']);
+Route::get('/list-cart', [CartController::class, 'getCart']);
+Route::get('/cart', [CartController::class, 'showCart']);
+

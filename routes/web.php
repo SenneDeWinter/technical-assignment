934c45d4cef8a9760af9b072fd366dcb7ad7d16d
SenneDeWinter <senne.dewinter@student.kdg.be>
2024-07-04 11:58:30 +0200

    🚀 Add routes for cart functionality and update comic route.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\ComicController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [ComicController::class, 'getComics']);
+Route::get('/', [ComicController::class, 'showComics']);
+
+Route::get('/list-comics', [ComicController::class, 'listComics']);
+Route::post('/add-to-cart/{comic_id}', [CartController::class, 'addToBasket']);
+Route::get('/cart', [CartController::class, 'getCart']);

f8a2e051c7ecc4fd53e3cc445844bacb14e9483d
SenneDeWinter <senne.dewinter@student.kdg.be>
2024-07-04 11:44:06 +0200

    🔄 ✨ Refactor ComicController methods for listing and showing comics

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -2,44 +2,24 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Comic;
-use GuzzleHttp\Client;
+use App\Http\Resources\ComicResource;
+use App\Repositories\Repositories\ComicRepository;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\View\View;
 
 class ComicController extends Controller
 {
-    public function retrieveComics(): void
+    public function listComics(): ResourceCollection
     {
-        $client = new Client();
-        $timestamp = time();
+        $comics = ComicRepository::getComics();
 
-        $response = $client->get(config('services.marvel.base_url') . '/comics', [
-            'query' => [
-                'apikey' => config('services.marvel.public_key'),
-                'ts' => $timestamp,
-                'hash' => md5($timestamp . config('services.marvel.private_key') . config('services.marvel.public_key')),
-                'limit' => 100,
-            ],
-        ]);
-
-        $comics = json_decode($response->getBody()->getContents(), true)['data']['results'];
-
-        foreach($comics as $comic){
-            $comicData = [
-                'title' => $comic['title'],
-                'format' => $comic['format'],
-                'marvel_id' => $comic['id'],
-                'page_count' => $comic['pageCount'],
-                'thumbnail' => $comic['thumbnail']['path'] . '.' . $comic['thumbnail']['extension'],
-            ];
-
-            Comic::updateOrCreate(['marvel_id' => $comicData['marvel_id']], $comicData);
-        }
+        return ComicResource::collection($comics);
     }
 
-    public function getComics()
+    public function showComics(): View
     {
-        $comics = Comic::paginate(6);
+        $comics = ComicRepository::getPaginatedComics();
 
-        return view('list', compact('comics'));
+        return view('list', ['comics' => $comics]);
     }
 }

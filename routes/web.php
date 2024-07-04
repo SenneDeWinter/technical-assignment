8ed2e9fdb2705353964325bd4f5d3d76754def0c
SenneDeWinter <senne.dewinter@student.kdg.be>
2024-07-04 16:45:21 +0200

    ✨ Add routes for Deleted Items Controller and reorganize existing routes.

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,18 @@
 
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\ComicController;
+use App\Http\Controllers\DeletedItemController;
 use Illuminate\Support\Facades\Route;
 
+//Comics
 Route::get('/', [ComicController::class, 'showComics']);
-
 Route::get('/list-comics', [ComicController::class, 'listComics']);
+
+//Cart
 Route::post('/add-to-cart/{comic_id}', [CartController::class, 'addToBasket']);
 Route::post('/remove-from-cart/{comic_id}', [CartController::class, 'removeFromBasket']);
 Route::get('/list-cart', [CartController::class, 'getCart']);
 Route::get('/cart', [CartController::class, 'showCart']);
+
+//Deleted Items
+Route::get('/deleted-items', [DeletedItemController::class, 'getDeletedItems']);
